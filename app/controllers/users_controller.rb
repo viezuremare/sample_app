49332bf475979e3a58ec9244171cbc9999ccdbef
viezure <viezuremare@gmx.com>
2019-04-26 20:38:44 +0300

    Implement basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,12 +10,19 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "Welcome"
       redirect_to @user
     else
       render 'new'
     end
-  end 
+  end
+  
+  def destroy
+    log_out
+    redirect_to root_url
+  end
+  
 
   private
 
